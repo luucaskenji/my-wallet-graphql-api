f3899e392d506ae0cba4bb5ca81ad2e8d132e0ef
Lucas Murakami <lkenjim98@gmail.com>
2021-05-15 18:25:08 -0300

    createUser mutation tests

--- __tests__/unit/resolvers.spec.ts
+++ __tests__/unit/resolvers.spec.ts
@@ -22,23 +22,180 @@ describe('resolvers', () => {
   describe('Mutations', () => {
     const { Mutation } = resolvers;
 
-    it('calls UserRepository method to save new user', async () => {
-      const UserRepositoryMock = { saveIfNotExists: jest.fn() };
-      mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+    describe('createUser', () => {
+      it('calls UserRepository method to save new user', async () => {
+        const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+        mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
 
-      const args: { input: createUserArgs} = {
-        input: {
-          firstName: 'First',
-          lastName: 'Last',
-          email: 'test@test.com',
-          password: 'test123456',
-          passwordConfirmation: 'test123456',
-        },
-      };
+        const args: { input: createUserArgs} = {
+          input: {
+            firstName: 'First',
+            lastName: 'Last',
+            email: 'test@test.com',
+            password: 'test123456',
+            passwordConfirmation: 'test123456',
+          },
+        };
 
-      await Mutation.createUser(null, args);
+        await Mutation.createUser(null, args);
 
-      expect(UserRepositoryMock.saveIfNotExists).toHaveBeenCalledWith(args.input);
+        expect(UserRepositoryMock.saveIfNotExists).toHaveBeenCalledWith(args.input);
+      });
+
+      describe('joi validation', () => {
+        it('throws error if some of the required fields are not sent', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = { // email field is missing
+            // @ts-ignore
+            input: {
+              firstName: 'First',
+              lastName: 'Last',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input firstName is empty', async () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: '',
+              lastName: 'Last',
+              email: 'test@test.com',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input firstName is longer than 15 characters', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'NameLongerThan15Characters',
+              lastName: 'Last',
+              email: 'test@test.com',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input firstName is empty', async () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: '',
+              email: 'test@test.com',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input lastName is longer than 15 characters', async () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: 'LastNameLongerThan15Characters',
+              email: 'test@test.com',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input email is not in email format', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: 'Last',
+              email: 'test.test.com',
+              password: 'test123456',
+              passwordConfirmation: 'test123456',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input password is shorter than 6 characters', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: 'Last',
+              email: 'test@test.com',
+              password: 'test',
+              passwordConfirmation: 'test',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input password is longer than 15 characters', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: 'Last',
+              email: 'test@test.com',
+              password: 'passwordLongerThan15Characters',
+              passwordConfirmation: 'passwordLongerThan15Characters',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+
+        it('throws error if input passwordConfirmation is not equal to password', () => {
+          const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+          mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
+
+          const args: { input: createUserArgs} = {
+            input: {
+              firstName: 'First',
+              lastName: 'Last',
+              email: 'test@test.com',
+              password: 'test123456',
+              passwordConfirmation: 'differentPassword',
+            },
+          };
+
+          expect(() => Mutation.createUser(null, args)).toThrow();
+        });
+      });
     });
   });
 });
