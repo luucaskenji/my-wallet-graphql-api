36ebc320a446b8734cf5ee35ad0ecba7f6aefe41
Lucas Murakami <lkenjim98@gmail.com>
2021-05-15 18:06:44 -0300

    configs update / createUser mutation first test

--- __tests__/unit/resolvers.spec.ts
+++ __tests__/unit/resolvers.spec.ts
@@ -1,15 +1,44 @@
-import { UserRepository } from '@/repositories';
+import { getCustomRepository } from 'typeorm';
+import { mocked } from 'ts-jest/utils';
 import resolvers from '@/resolvers';
+import { createUserArgs } from '@/types/resolvers';
+
+jest.mock('typeorm', () => ({
+  getCustomRepository: jest.fn(),
+  Entity: jest.fn(),
+  PrimaryGeneratedColumn: jest.fn(),
+  Column: jest.fn(),
+  CreateDateColumn: jest.fn(),
+  UpdateDateColumn: jest.fn(),
+  BeforeInsert: jest.fn(),
+  BeforeUpdate: jest.fn(),
+  ManyToOne: jest.fn(),
+  OneToMany: jest.fn(),
+  Repository: class Mock { },
+  EntityRepository: jest.fn(),
+}));
 
 describe('resolvers', () => {
   describe('Mutations', () => {
     const { Mutation } = resolvers;
-    const userRepository = new UserRepository();
 
-    it('calls UserRepository method to save new user', () => {
-      jest.spyOn(userRepository, 'saveIfNotExists');
+    it('calls UserRepository method to save new user', async () => {
+      const UserRepositoryMock = { saveIfNotExists: jest.fn() };
+      mocked(getCustomRepository).mockReturnValueOnce(UserRepositoryMock);
 
-      
+      const args: { input: createUserArgs} = {
+        input: {
+          firstName: 'First',
+          lastName: 'Last',
+          email: 'test@test.com',
+          password: 'test123456',
+          passwordConfirmation: 'test123456',
+        },
+      };
+
+      await Mutation.createUser(null, args);
+
+      expect(UserRepositoryMock.saveIfNotExists).toHaveBeenCalledWith(args.input);
     });
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      branches: 80,
+      functions: 80,
+    },
+  },
   coveragePathIgnorePatterns: [
     '<rootDir>/database/*',
     '<rootDir>/models/*',
@@ -8,8 +15,13 @@ module.exports = {
     '<rootDir>/validations/*',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['/__tests__/**/*.spec.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  setupFiles: [
+    '<rootDir>/.jest/typeormMocks.js',
+  ],
 };
